2c1f3f670c016ad56231b2a3a99e713e64a8ab0b
gokul <mv.gokul4@gmail.com>
2020-06-16 18:01:45 +0200

    new

--- src/utils/dateFormat.js
+++ src/utils/dateFormat.js
@@ -1,5 +1,5 @@
 import  moment from "moment"
 
 export const Datetime=(date)=>{
-    return moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a")
+    return moment(date).format("dddd, MMMM Do YYYY, h:mm a")
 }
